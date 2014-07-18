d63a078a5056af78d26c7400cf44af479c6581d6
Brian Sorahan <bsorahan@gmail.com>
2014-07-17 21:54:33 -0500

    cleaning up provision script

--- provision.sh
+++ provision.sh
@@ -1,16 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 SYSTEMS='ubuntu debian'
 
-if [ -z "$1" ]; then
+function usage_and_exit {
     echo 1>&2 'Usage:'
-    echo 1>&2 "$0" system
+    echo 1>&2 "$0" '<system type>'
     echo 1>&2
     echo 'Valid systems: ' 1>&2
     for s in $SYSTEMS; do
         echo "   $s" 1>&2
     done
     echo 1>&2
+    exit 1
+}
+
+if [ -z "$1" ]; then
+    usage_and_exit
 fi
 
 case "$1" in
@@ -23,10 +28,6 @@ case "$1" in
     *)
         echo 1>&2 Unrecognized system: "$1"
         echo 1>&2
-        echo 'Valid systems: ' 1>&2
-        for s in $SYSTEMS; do
-            echo "   $s" 1>&2
-        done
-        echo 1>&2
+        usage_and_exit
         ;;
 esac
