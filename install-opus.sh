e6f5b6151ee7fedb3fe64244e905c86f7077868a
Brian Sorahan <bsorahan@gmail.com>
2014-09-07 20:21:26 -0500

    serialize install-opus commands

--- install-opus.sh
+++ install-opus.sh
@@ -8,8 +8,8 @@ function main {
     local ogg=http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
     local opus=http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
     local opustools=https://ftp.mozilla.org/pub/mozilla.org/opus/opus-tools-0.1.9.tar.gz
-    pkg_install $ogg &
-    pkg_install $opus &
+    pkg_install $ogg
+    pkg_install $opus
     pkg_install $opustools
 }
 
