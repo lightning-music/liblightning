2afce8eb6fc193001bf765fbe0995571aebf8773
Brian Sorahan <bsorahan@gmail.com>
2014-09-16 22:39:24 -0500

    fix arch build

--- flags.mk
+++ flags.mk
@@ -1,9 +1,9 @@
 LIGHTNING_FLAGS := $(shell pkg-config --cflags --libs jack sndfile samplerate check ogg opus)
 CC=gcc
 ifeq ($(DEBUG),true)
-CFLAGS := -Wall -g -std=c99
+CFLAGS := -Wall -g
 else
-CFLAGS := -Wall -O2 -std=c99
+CFLAGS := -Wall -O2
 endif
 LDFLAGS := -L.
-LDLIBS := -llightning $(LIGHTNING_FLAGS)
+LDLIBS := -llightning $(LIGHTNING_FLAGS) -lrt -lm
