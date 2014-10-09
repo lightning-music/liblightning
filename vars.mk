1bb0b531fe6bc4c89347a82ca83d89fa711f7077
Brian Sorahan <bsorahan@gmail.com>
2014-10-08 22:21:42 -0500

    back out of autohell

---
+++ vars.mk
@@ -0,0 +1,16 @@
+prefix=/usr/local
+bindir=$(prefix)/bin
+includedir=$(prefix)/include/lightning
+libdir=$(prefix)/lib
+# headers
+HEADERS = lightning.h types.h
+# examples, archive
+LIBLIGHTNING_AR=liblightning.a
+EXAMPLES := play-file play-sample lightning-play-sample
+EXAMPLES := $(addprefix examples/,$(EXAMPLES))
+# tests
+TEST_DIR=test
+TESTS := check-list check-metro
+TESTS := $(addprefix $(TEST_DIR)/, $(TESTS))
+# API docs
+API_DOCS_DIR=/var/www/html/liblightning
