3168230ebb16477892c5ab490d83682d677b3689
Brian Sorahan <bsorahan@gmail.com>
2015-01-18 23:35:11 -0600

    actually include .

--- flags.mk
+++ flags.mk
@@ -1,6 +1,5 @@
 LIGHTNING_FLAGS=$(shell pkg-config --cflags --libs jack sndfile samplerate check)
 CC=gcc
-CPPFLAG=-I.
-CFLAGS=-Wall -g -O2
+CFLAGS=-I. -Wall -g -O2
 LDFLAGS=-L.
 LDLIBS=-llightning $(LIGHTNING_FLAGS) -lrt -lm
